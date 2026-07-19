4cd3aaf9436a51a3f6db1aea1a49206123e58f2c
goldlamha33-art <goldlamha33@gmail.com>
2026-07-19 13:04:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from playwright.sync_api import sync_playwright
 import time
+URL = "https://www.next.co.uk/clearance/search?w=women&p=1&af=category:dresses%20gender:women%20size:10%20size:8&isort=price#0"
+
+
+def main():
